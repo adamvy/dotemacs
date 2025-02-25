8e2424325741a240a1d38bb2e37fec67dfe37c13
Adam Van Ymeren <adam@vany.ca>
2025-02-25 13:41:35 -0800

    add grok to gptel

--- init.el
+++ init.el
@@ -10,7 +10,6 @@
    '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af" "98b4ef49c451350c28a8c20c35c4d2def5d0b8e5abbc962da498c423598a1cdd" "6ac2faf17d4d37b6f4bc08203b70e82f4b3b5ce76f102fb4802b3f6c74460743" "1d1f4f5b0f792f0bb1b8f944b8ed93b3b20bbebc4ba072c2b7daff82da23ae86" "039112154ee5166278a7b65790c665fe17fd21c84356b7ad4b90c29ffe0ad606" "f700bc979515153bef7a52ca46a62c0aa519950cc06d539df4f3d38828944a2c" default))
  '(epg-pinentry-mode 'loopback)
  '(global-undo-tree-mode t)
- '(gptel-model 'gpt-4o)
  '(ido-everywhere t)
  '(ido-mode 'both nil (ido))
  '(js-indent-level 2)
@@ -19,7 +18,7 @@
      ("gnu" . "https://elpa.gnu.org/packages/")
      ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
  '(package-selected-packages
-   '(auth-source-1password spacemacs-theme olivetti nord-theme undo-tree visual-fill visual-fill-column base16-theme magit eglot-java eglot dape helm-project helm-lsp helm lsp-java dap-mode gptel llm projectile project))
+   '(gptel auth-source-1password spacemacs-theme olivetti nord-theme undo-tree visual-fill visual-fill-column base16-theme magit eglot-java eglot dape helm-project helm-lsp helm lsp-java dap-mode llm projectile project))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -32,13 +31,21 @@
 
 (require 'eglot)
 (let ((jdtls-path "~/.emacs.d/jdtls/bin/jdtls")
-        (java-debug-jar (car (file-expand-wildcards "~/.emacs.d/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"))))
-    (add-to-list 'eglot-server-programs
-                 `((java-mode java-ts-mode)
-                   . (,jdtls-path
-                      :initializationOptions
-                      (:bundles [,java-debug-jar])))))
+      (java-debug-jar (car (file-expand-wildcards "~/.emacs.d/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"))))
+  (add-to-list 'eglot-server-programs
+               `((java-mode java-ts-mode)
+                 . (,jdtls-path
+                    :initializationOptions
+                    (:bundles [,java-debug-jar])))))
 
-
-       
+(require 'auth-source-1password)
 (auth-source-1password-enable)
+
+(require 'gptel)
+(gptel-make-openai "grok"
+  :host "api.x.ai"
+  :key #'gptel-api-key-from-auth-source
+  :endpoint "/v1/chat/completions"
+  :stream t
+  :models '(grok-2-1212))
+
