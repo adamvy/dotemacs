a1c60ec5413030880bf724e12caac7eec0a0d89b
Adam Van Ymeren <adam@vany.ca>
2025-02-26 06:45:11 -0800

    fonts

--- init.el
+++ init.el
@@ -25,7 +25,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Menlo" :foundry "nil" :slant normal :weight medium :height 120 :width normal)))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#fbf8ef" :foreground "#655370" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight medium :height 120 :width normal :foundry "nil" :family "Source Code Pro"))))
+ '(variable-pitch ((t (:family "Georgia")))))
 
 (package-activate-all)
 
